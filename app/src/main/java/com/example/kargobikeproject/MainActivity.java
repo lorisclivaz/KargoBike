b7d037e64ff2ccfe8ab4beab9bb287b45335fdeb
Loris_Clivaz <loris.clivaz@students.hevs.ch>
2019-11-20 10:15:41 +0100

    Layout about CheckBike and GpsCheckPoint loris

--- app/src/main/java/com/example/kargobikeproject/MainActivity.java
+++ app/src/main/java/com/example/kargobikeproject/MainActivity.java
@@ -9,12 +9,16 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 Button buttonAddProduct;
+Button AddGpsCheckpoint;
+Button checkBike;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonAddProduct=findViewById(R.id.buttonAddProduct);
+        AddGpsCheckpoint=findViewById(R.id.buttonGps);
+        checkBike=findViewById(R.id.buttonCheckBike);
 
 
         buttonAddProduct.setOnClickListener(new View.OnClickListener() {
@@ -24,14 +28,24 @@ Button buttonAddProduct;
             }
         });
 
+        AddGpsCheckpoint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GpsCheckPointActivity.class));
+            }
+        });
 
+        checkBike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CheckBikeActivity.class));
+            }
+        });
     }
 
 
 
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, AddProductActivity.class);
-    }
+
 
 
 
