ab6c81afa527d37c8a27fcb079e1356a5928c04e
Loris_Clivaz <loris.clivaz@students.hevs.ch>
2019-11-25 11:40:14 +0100

    Adding order database loris

--- app/src/main/java/com/example/kargobikeproject/AddOrderActivity.java
+++ app/src/main/java/com/example/kargobikeproject/AddOrderActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddOrderActivity extends AppCompatActivity {
 
@@ -17,5 +18,9 @@ public class AddOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_order);
+
+
+        FirebaseDatabase.getInstance().getReference().push()
+                .child("Order").setValue(new Order(1,1,1,1,"Chemin des plannetes 20","12.12.2019","14.12.2019",1));
     }
 }
--- app/src/main/java/com/example/kargobikeproject/DatabaseMethode/asdfasf.java
+++
@@ -1,4 +0,0 @@
-package com.example.kargobikeproject.DatabaseMethode;
-
-public class asdfasf {
-}
