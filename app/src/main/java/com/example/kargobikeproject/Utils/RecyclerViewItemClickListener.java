d87830de31a21f4e6acdadb1ec105e799907ae71
Romain <Romain@LAPTOP-LO6E8S0P>
2019-11-28 11:14:39 +0100

    First Layout : add a product

---
+++ app/src/main/java/com/example/kargobikeproject/Utils/RecyclerViewItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.kargobikeproject.Utils;
+
+import android.view.View;
+
+public interface RecyclerViewItemClickListener {
+    void onItemClick(View v, int position);
+    void onItemLongClick(View v, int position);
+}
