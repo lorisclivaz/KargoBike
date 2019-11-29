45745afa541ae530451d9a07d3261c767cdf3f3f
loic <loic.python@gmail.com>
2019-11-29 09:16:38 +0100

    Authorize user for the superuser 2

--- app/src/main/java/com/example/kargobikeproject/AddAuthorizedUser.java
+++ app/src/main/java/com/example/kargobikeproject/AddAuthorizedUser.java
@@ -6,31 +6,67 @@ import android.widget.Spinner;
 
 import com.example.kargobikeproject.Model.Repository.OrderRepository;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddAuthorizedUser extends AppCompatActivity {
+
+    private DatabaseReference mDatabaseReference;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> arrayUser;
+    ValueEventListener listener;
+    Spinner spinnerUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_authorized_user);
-        Spinner spinnerUser = (Spinner) findViewById(R.id.spinnerUser);
+        spinnerUser = (Spinner) findViewById(R.id.spinnerUserAuth);
 
-        final ArrayList<String> arrayUser = new ArrayList<>();
-        arrayUser.add("Solange");
-        arrayUser.add("Loris");
-        arrayUser.add("Nghi");
-        arrayUser.add("Romain");
-        arrayUser.add("Loic");
+        spinnerUser.setPrompt("Select the user");
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference("users");
 
-        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayUser);
+
+       /* ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayUser);
 
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerUser.setAdapter(spinnerArrayAdapter);
+        spinnerUser.setAdapter(spinnerArrayAdapter);*/
+
+        arrayUser  = new ArrayList<>();
+        adapter = new ArrayAdapter<String>(AddAuthorizedUser.this,android.R.layout.simple_spinner_dropdown_item, arrayUser);
+
+        spinnerUser.setAdapter(adapter);
+    }
+
+
+    public void retrieveData()
+    {
+        listener = mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot item:dataSnapshot.getChildren()){
+
+                    arrayUser.add(item.getValue().toString());
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
