2f430b51fb30c67c8857b55127b20810c13e4c31
RomainCastagna <romaincastagna1995@gmail.com>
2019-12-01 21:46:42 +0100

    NavBar

---
+++ app/src/main/java/com/example/kargobikeproject/ui/orders/OrderViewModel.java
@@ -0,0 +1,7 @@
+package com.example.kargobikeproject.ui.orders;
+
+import androidx.lifecycle.ViewModel;
+
+public class OrderViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/kargobikeproject/ui/products/ProductFragment.java
@@ -0,0 +1,116 @@
+package com.example.kargobikeproject.ui.products;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.kargobikeproject.Adapter.ProductAdapter;
+import com.example.kargobikeproject.AddProductActivity;
+import com.example.kargobikeproject.Model.Entity.Product;
+import com.example.kargobikeproject.ProductDetailActivity;
+import com.example.kargobikeproject.R;
+import com.example.kargobikeproject.Utils.RecyclerViewItemClickListener;
+import com.example.kargobikeproject.ViewModel.ProductListViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
+public class ProductFragment extends Fragment {
+
+    private ProductViewModel mViewModel;
+    private List<Product> products;
+    private ProductAdapter recyclerAdapter;
+    private ProductListViewModel viewModel;
+
+    public static ProductFragment newInstance() {
+        return new ProductFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.product_fragment, container, false);
+
+        return inflater.inflate(R.layout.product_fragment, container, false);
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        RecyclerView recyclerView =getView().findViewById(R.id.productsRecyclerView);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                LinearLayoutManager.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
+        products = new ArrayList<>();
+        recyclerAdapter = new ProductAdapter(new RecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Log.d(TAG, "clicked position:" + position);
+                Log.d(TAG, "clicked on: " + products.get(position).toString());
+
+                Intent intent = new Intent(getActivity(), ProductDetailActivity.class);
+                intent.setFlags(
+                        Intent.FLAG_ACTIVITY_NO_ANIMATION |
+                                Intent.FLAG_ACTIVITY_NO_HISTORY
+                );
+                intent.putExtra("IdProduct", products.get(position).getId());
+                startActivity(intent);
+            }
+
+            @Override
+            public void onItemLongClick(View v, int position) {
+                Log.d(TAG, "longClicked position:" + position);
+                Log.d(TAG, "longClicked on: " + products.get(position).toString());
+            }
+        });
+
+        FloatingActionButton fab = getView().findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(view -> {
+                    Intent intent = new Intent(getActivity(), AddProductActivity.class);
+                    intent.setFlags(
+                            Intent.FLAG_ACTIVITY_NO_ANIMATION |
+                                    Intent.FLAG_ACTIVITY_NO_HISTORY
+                    );
+                    startActivity(intent);
+                }
+        );
+        ProductListViewModel.Factory factory = new ProductListViewModel.Factory(getActivity().getApplication());
+        viewModel = ViewModelProviders.of(this, factory).get(ProductListViewModel.class);
+        viewModel.getBeverages().observe(this, productEntities -> {
+            if (productEntities != null) {
+                products = productEntities;
+                recyclerAdapter.setData(products);
+            }
+        });
+
+        recyclerView.setAdapter(recyclerAdapter);
+
+
+
+
+
+
+    }
+
+}
