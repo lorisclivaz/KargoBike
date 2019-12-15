b35565793b8619f25dcff42acbc1c469f748f8ed
solst2 <47793395+solst2@users.noreply.github.com>
2019-12-15 18:57:34 +0100

    undo not needed changes

--- app/src/main/java/com/group3/kargobikeproject/Model/Repository/CheckPointRepository.java
+++ app/src/main/java/com/group3/kargobikeproject/Model/Repository/CheckPointRepository.java
@@ -1,39 +1,12 @@
 package com.group3.kargobikeproject.Model.Repository;
 
-import com.google.firebase.database.DatabaseReference;
+
 import com.group3.kargobikeproject.Model.Entity.CheckPoint;
-import com.group3.kargobikeproject.Model.Entity.User;
-import com.group3.kargobikeproject.Model.Firebase.CheckPointListLiveData;
-import com.group3.kargobikeproject.Model.Firebase.UserListLiveData;
 import com.group3.kargobikeproject.Utils.OnAsyncEventListener;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.List;
-
-import androidx.lifecycle.LiveData;
 
 public class CheckPointRepository {
-
-    private static CheckPointRepository instance;
-
-    private CheckPointRepository(){
-    }
-
-    public static CheckPointRepository getInstance() {
-        if (instance == null) {
-            synchronized (UserRepository.class) {
-                if (instance == null) {
-                    instance = new CheckPointRepository();
-                }
-            }
-        }
-        return instance;
-    }
-
-    public LiveData<List<CheckPoint>> getAllCheckpoints() {
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("checkPoint");
-        return new CheckPointListLiveData(reference);
-    }
 
     public void insert(final CheckPoint checkPoint, final OnAsyncEventListener callback) {
         String id = FirebaseDatabase.getInstance().getReference("checkPoint").push().getKey();
