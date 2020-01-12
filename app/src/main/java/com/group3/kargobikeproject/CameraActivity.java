def14c22a4c8d354d1f101548e02476e307ed205
mia <genttunn@gmail.com>
2020-01-12 18:17:05 +0100

    increased camera image quality

--- app/src/main/java/com/group3/kargobikeproject/CameraActivity.java
+++ app/src/main/java/com/group3/kargobikeproject/CameraActivity.java
@@ -2,6 +2,7 @@ package com.group3.kargobikeproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -12,6 +13,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Base64;
 import android.util.Log;
@@ -32,6 +34,7 @@ import com.group3.kargobikeproject.ui.orders.OrderFragment;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -45,6 +48,7 @@ public class CameraActivity extends AppCompatActivity {
     String newPicFile = idOrderThis + ".jpg";
     StorageReference storageRef;
     FirebaseStorage storage;
+    String currentPhotoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,9 +62,10 @@ public class CameraActivity extends AppCompatActivity {
         storage = FirebaseStorage.getInstance();
         openCameraButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, 1);
-            }
+//                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(cameraIntent, 1);
+                dispatchTakePictureIntent();
+        }
         });
         saveButtonCamera.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -69,6 +74,10 @@ public class CameraActivity extends AppCompatActivity {
         });
         cancelButtonCamera.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if(currentPhotoPath!=null){
+                    File file = new File(currentPhotoPath);
+                    file.delete();
+                }
                 Intent intent = new Intent(CameraActivity.this, OrderCheckpointActivity.class);
                 intent.putExtra("ORDER_ID",idOrderThis);
                 startActivity(intent);
@@ -98,10 +107,10 @@ public class CameraActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if (requestCode == 1  && resultCode == RESULT_OK) {
             saveButtonCamera.setEnabled(true);
-            Bitmap image = (Bitmap) data.getExtras().get("data");
-            tempBitmap = image;
-            BitmapDrawable bdrawable = new BitmapDrawable(this.getResources(),image);
-            mContent.setImageBitmap(image);
+            Bitmap image = BitmapFactory.decodeFile(currentPhotoPath);
+            tempBitmap = setPic(mContent,currentPhotoPath);
+//            BitmapDrawable bdrawable = new BitmapDrawable(this.getResources(),image);
+//            mContent.setImageBitmap(image);
         }
     }
     public void saveAndUpload(Bitmap imageToUpload) {
@@ -126,9 +135,13 @@ public class CameraActivity extends AppCompatActivity {
                 @Override
                 public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
                     Task<Uri> uri = taskSnapshot.getStorage().getDownloadUrl();
-                    // while(!uri.isComplete());
+                    while(!uri.isComplete());
                     Uri url = uri.getResult();
                     Log.v("image url", url.toString());
+                    if(currentPhotoPath!=null){
+                        File file = new File(currentPhotoPath);
+                        file.delete();
+                    }
                 }
             });
             Intent intent = new Intent(CameraActivity.this, MenuFragementActivity.class);
@@ -171,5 +184,64 @@ public class CameraActivity extends AppCompatActivity {
             bmImage.setBackground(bdrawable);
             bmImage.setImageBitmap(result);
         }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+            }
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.group3.kargobikeproject.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, 1);
+            }
+        }
+    }
+
+    private Bitmap setPic(ImageView imageView, String currentPhotoPath) {
+        // Get the dimensions of the View
+        int targetW = imageView.getWidth();
+        int targetH = imageView.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+        imageView.setImageBitmap(bitmap);
+        return bitmap;
     }
 }
